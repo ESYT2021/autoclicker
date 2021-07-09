4e4c7c652fe1c1775f42a90acc5ec3f08b3216cd
Stealth <47611754+ESYT2021@users.noreply.github.com>
2021-07-09 10:16:31 +0100

    Update AssemblyInfo.cs

--- autoclicker/Properties/AssemblyInfo.cs
+++ autoclicker/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AClick")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright© Stealthh 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
